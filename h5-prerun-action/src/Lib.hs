7607d75b2f11cfc78f0661b3806bb16a44e79bda
Naïm Favier <n@monade.li>
2024-05-09 12:46:59 +0200

    h5

--- h5-prerun-action/src/Lib.hs
+++ h5-prerun-action/src/Lib.hs
@@ -1,6 +1,13 @@
+{-# LANGUAGE BlockArguments #-}
 module Lib
     ( prerun
     ) where
 
+import Control.Concurrent.MVar
+import Control.Monad
+
 prerun :: (IO a -> IO (IO b)) -> IO (IO a -> IO b)
-prerun = undefined
+prerun f = do
+    r <- newEmptyMVar
+    b <- f (join (takeMVar r))
+    pure \ a -> putMVar r a >> b
