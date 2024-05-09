31390a332aa050207ddd7f1d3e22e540f292397e
Naïm Favier <n@monade.li>
2024-05-09 12:19:35 +0200

    h3, h7

--- h3-transform-typed/src/Lib.hs
+++ h3-transform-typed/src/Lib.hs
@@ -10,10 +10,15 @@ data Scheme a where
     Arg :: Typeable a => Proxy a -> Scheme b -> Scheme (a -> b)
 
 data Function = forall a. Function (Scheme a) a
+data Transform a = forall b. Transform (Scheme b) (a -> b)
 
 newtype Wrap a = Wrap
     { unWrap :: a
     }
 
+transform :: Scheme a -> Transform a
+transform (Res _) = Transform (Res Proxy) Wrap
+transform (Arg _ s) = case transform s of Transform s' t -> Transform (Arg Proxy s') (\f -> t . f . unWrap)
+
 wrapFunction :: Function -> Function
-wrapFunction = undefined
+wrapFunction (Function s f) = case transform s of Transform s' t -> Function s' (t f)
--- h7-shortest-longest/src/Lib.hs
+++ h7-shortest-longest/src/Lib.hs
@@ -2,5 +2,19 @@ module Lib
     ( shortestLongest
     ) where
 
+import Control.Applicative
+import Data.Maybe
+
+steps = go []
+  where
+    go l (x:xs) = Nothing : go (x:l) xs
+    go l [] = Just [reverse l] : repeat (Just [])
+
+traverseThen f g = fmap f . traverse g
+
 shortestLongest :: [[[a]]] -> [[a]]
-shortestLongest = undefined
+shortestLongest = head
+                . catMaybes
+                . getZipList
+                . traverseThen mconcat (traverseThen (fmap concat . sequenceA)
+                                                     (ZipList . steps))
